ad881b6b89e0109d31a99843d9979eb96a79d927
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-08-31 13:58:23 +0300

    added the notes div and suggestions

---
+++ assets/index.js
@@ -0,0 +1,9 @@
+
+document.getElementById("showNotes").style.display="none";
+function showNotes() {
+  document.getElementById("showNotes").style.display="block";
+  }
+  document.getElementById("querries").style.display="none";
+function showQuestions() {
+  document.getElementById("querries").style.display="block";
+  }
